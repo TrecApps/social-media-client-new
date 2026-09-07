325c3d6dd0c864e9f98466010f119152033bbb24
John Jacko <jljacko@outlook.com>
2026-09-07 10:16:09 -0600

    Prepared for test deployment

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -4,12 +4,15 @@ export const environment = {
     app_name: "Coffeeshop",
   logo_image: "assets/Logos/Coffee-Shop-Logo.png",
 
-    user_service_url: "http://localhost:4200/User-api",
-    image_service_url: "http://localhost:4200/Image-man-api",
-    sm_profile_url: 'http://localhost:4200/sm-api/',
-    sm_content_url: 'http://localhost:4200/sm-api/',
-    resource_service_url: "http://localhost:4200/Falsehoods-api/",
+    // user_service_url: "http://localhost:4200/User-api",
+    // image_service_url: "http://localhost:4200/Image-man-api",
+    // sm_profile_url: 'http://localhost:4200/sm-api/',
+    // sm_content_url: 'http://localhost:4200/sm-api/',
+    // resource_service_url: "http://localhost:4200/Falsehoods-api/",
 
-    // user_service_url: "https://test.trecapps.com/User-api",
-    // image_service_url: "https://test.trecapps.com/Image-man-api"
+    user_service_url: "https://test.trecapps.com/User-api",
+    image_service_url: "https://test.trecapps.com/Image-man-api",
+    sm_profile_url: 'https://test.trecapps.com/sm-api/',
+    sm_content_url: 'https://test.trecapps.com/sm-api/',
+    resource_service_url: "https://test.trecapps.com/Falsehoods-api/",
 } 
